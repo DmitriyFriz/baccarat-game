1f6ae023e6782eb58a3d78be88cba4d727ab15c6
DmitriyFriz <dmitriy.bulavko.friz44@gmail.com>
2021-07-03 00:56:02 +0300

    feat: add deck icon

--- src/components/Board/Board.tsx
+++ src/components/Board/Board.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { observer } from 'mobx-react-lite';
 import { useStore } from '../../store';
 import { CardCollection } from '../CardCollection';
+import { ReactComponent as DeckIcon } from '../../assets/img/deck.svg';
 
 import s from './Board.module.scss';
 
@@ -12,6 +13,7 @@ export const Board = observer(() => {
     <div className={s.board}>
       <CardCollection collection={gameStore.playerCards} isBankerCollection={false} />
       <CardCollection collection={gameStore.bankerCards} isBankerCollection={true} />
+      <DeckIcon className={s.deck} />
     </div>
   );
 });
