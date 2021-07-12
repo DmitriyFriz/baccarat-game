49e894e9d88e7adc3da9c81b34b6b9e2e86b25d7
DmitriyFriz <dmitriy.bulavko.friz44@gmail.com>
2021-07-12 23:10:25 +0300

    feat: add zoom for chart

--- src/components/BarChart/BarChart.tsx
+++ src/components/BarChart/BarChart.tsx
@@ -1,21 +1,44 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import { toJS } from 'mobx';
 import { observer } from 'mobx-react-lite';
 import { Bar } from 'react-chartjs-2';
+import { Chart } from 'chart.js';
+import zoomPlugin from 'chartjs-plugin-zoom';
 import { useStore } from '../../store';
-import { getDataConfig, optionsConfig } from './chartConfig';
+import { getChartConfig } from './chartConfig';
 
 import style from './BarChart.module.scss';
+
+Chart.register(zoomPlugin);
+
+interface Zoom {
+  resetZoom: () => void;
+}
 
 export const BarChart = observer(() => {
   const statisticsStore = useStore('statisticsStore');
   const chartData = toJS(statisticsStore.chartData);
 
+  const chartRef = useRef<Zoom>(null);
+
+  const { dataConfig, optionsConfig } = getChartConfig(chartData);
+
   return (
-    <div className={style.container}>
+    <>
       <div className={style.chartWrapper}>
-        <Bar data={getDataConfig(chartData)} options={optionsConfig} type="bar" />
+        <Bar ref={chartRef} data={dataConfig} options={optionsConfig} type="bar" />
       </div>
-    </div>
+      <button
+        type="button"
+        className={`button ${style.resetButton}`}
+        onClick={() => {
+          if (chartRef.current !== null) {
+            chartRef.current.resetZoom();
+          }
+        }}
+      >
+        Reset Zoom
+      </button>
+    </>
   );
 });
--- src/components/BarChart/chartConfig.ts
+++ src/components/BarChart/chartConfig.ts
@@ -3,11 +3,23 @@ interface ChartDataItem {
   date: string;
 }
 
-export const getDataConfig = (chartData: ChartDataItem[]) => {
+const zoomFactor = 1.1;
+
+export const getChartConfig = (chartData: ChartDataItem[]) => {
   const labels = chartData.map(({ date }) => date);
   const data = chartData.map(({ value }) => value);
-  const colours = chartData.map(({ value }) => (value < 0 ? 'red' : 'green'));
+  const colours = chartData.map(({ value }) => (value < 0 ? '#ff6347' : '#7ae67a'));
 
+  const minZoom = Math.ceil(Math.min(...data) * zoomFactor);
+  const maxZoom = Math.ceil(Math.max(...data) * zoomFactor);
+
+  return {
+    dataConfig: getDataConfig(labels, data, colours),
+    optionsConfig: getOptionsConfig(minZoom, maxZoom),
+  };
+};
+
+const getDataConfig = (labels: string[], data: number[], colours: string[]) => {
   return {
     labels,
     datasets: [
@@ -19,10 +31,29 @@ export const getDataConfig = (chartData: ChartDataItem[]) => {
   };
 };
 
-export const optionsConfig = {
+const getOptionsConfig = (minZoom: number, maxZoom: number) => ({
   plugins: {
     legend: {
       display: false,
+    },
+    zoom: {
+      pan: {
+        enabled: true,
+      },
+
+      zoom: {
+        wheel: {
+          enabled: true,
+        },
+        pinch: {
+          enabled: true,
+        },
+        mode: 'xy',
+      },
+
+      limits: {
+        y: { min: minZoom, max: maxZoom },
+      },
     },
   },
   scales: {
@@ -35,4 +66,10 @@ export const optionsConfig = {
       reverse: true,
     },
   },
-};
+  pan: {
+    enabled: true,
+    mode: 'xy',
+    speed: 1,
+    threshold: 1,
+  },
+});
