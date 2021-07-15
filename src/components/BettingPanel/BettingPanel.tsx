1eb71dfda7b073de930d7d32d7f700e12b79b733
DmitriyFriz <dmitriy.bulavko.friz44@gmail.com>
2021-07-15 21:18:45 +0300

    style: hide betting panel during game

--- src/components/BettingPanel/BettingPanel.tsx
+++ src/components/BettingPanel/BettingPanel.tsx
@@ -1,19 +1,40 @@
-import React from 'react';
+import React, { useRef } from 'react';
+import { observer } from 'mobx-react-lite';
+import { CSSTransition } from 'react-transition-group';
+import { useStore } from '../../store';
 import { BettingPlace } from '../BettingPlace';
 import { BettingName } from '../../common/gameData';
 import { BettingControllers } from '../BettingControllers';
 
 import style from './BettingPanel.module.scss';
 
-export const BettingPanel = () => {
+export const BettingPanel = observer(() => {
+  const bettingStore = useStore('bettingStore');
+
+  const nodeRef = useRef(null);
+
   return (
-    <>
-      <div className={style.places}>
-        <BettingPlace bettingName={BettingName.Player} />
-        <BettingPlace bettingName={BettingName.Tie} />
-        <BettingPlace bettingName={BettingName.Banker} />
+    <CSSTransition
+      classNames={{
+        enterActive: style.panelShow,
+        exitActive: style.panelHidden,
+      }}
+      in={!bettingStore.isLockedBet}
+      timeout={{
+        enter: 3000,
+        exit: 700,
+      }}
+      unmountOnExit
+      nodeRef={nodeRef}
+    >
+      <div className={style.panel} ref={nodeRef}>
+        <div className={style.places}>
+          <BettingPlace bettingName={BettingName.Player} />
+          <BettingPlace bettingName={BettingName.Tie} />
+          <BettingPlace bettingName={BettingName.Banker} />
+        </div>
+        <BettingControllers />
       </div>
-      <BettingControllers />
-    </>
+    </CSSTransition>
   );
-};
+});
